e633f0f53bdf67f1d9cf7b6e53444f1d4c6d91de
bouzal <49028204+Bouzaloussama@users.noreply.github.com>
2021-04-12 18:17:27 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/partials/Navbar';
-import Al_3ossor from './components/diwan/Al_3ossor';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Users from './components/users/Users';
@@ -18,9 +17,7 @@ function App() {
         <Switch>
           <Route exact path="/users" component={Users}/>
           <Route exact path="/users/:login" component={UserItem}/>
-          <Route exact component={Users}/>{/*
-          <Route exact path="/Al_3ossor" component={Al_3ossor}/>
-          <Route exact component={Al_3ossor}/>*/}
+          <Route exact component={Users}/>
         </Switch>
       </div>
     </div>
